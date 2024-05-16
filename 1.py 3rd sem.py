a2af94838eeebd14708373566d9ebedafe610a00
kar143143 <165043950+kar143143@users.noreply.github.com>
2024-05-16 09:46:32 +0530

    Add files via upload

---
+++ 1.py 3rd sem.py
@@ -0,0 +1,20 @@
+val=10;val+=20;val-=2;val*=2;val//=5;val%=7;val**=3;val/=3
+
+print("The value of the e variable val",val,sep="=")
+
+10+20-3**2*4-10*(3+2)//3/2%5
+
+print("The value of the Expression 10+20-3**2*4-10*(3+2)//3/2%5",\
+10+20-3**2*4-10*(3+2)//3/2%5,sep="=")
+      
+val=10
+val+=20
+val-=2
+val*=2
+val//=5
+val%=7
+val**=3
+val/=3
+
+print("the value of the e variable val",val,sep="=")
+
